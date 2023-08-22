04739f1debff7317218f52d2c3dbd91c9c79e54b
dthuyhuynh <dthuyhuynh901.com>
2023-08-22 10:33:11 +0700

    register repository

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,14 +2,30 @@
 
 namespace App\Providers;
 
+use App\Repositories\Address\AddressRepository;
+use App\Repositories\Address\AddressRepositoryInterface;
+use App\Repositories\Brand\BrandRepo;
+use App\Repositories\Brand\BrandRepoInterface;
 use App\Repositories\CartItem\CartItemRepository;
 use App\Repositories\CartItem\CartItemRepositoryInterface;
 use App\Repositories\Category\CategoryRepository;
 use App\Repositories\Category\CategoryRepositoryInterface;
+use App\Repositories\Order\OrderRepository;
+use App\Repositories\Order\OrderRepositoryInterface;
+use App\Repositories\OrderDetail\OrderDetailRepository;
+use App\Repositories\OrderDetail\OrderDetailRepositoryInterface;
+use App\Repositories\Payment\PaymentRepository;
+use App\Repositories\Payment\PaymentRepositoryInterface;
 use App\Repositories\Product\ProductRepositoryInterface;
 use App\Repositories\Product\ProductRepository;
 use App\Repositories\ProductComment\ProductCommentRepository;
 use App\Repositories\ProductComment\ProductCommentRepositoryInterface;
+use App\Repositories\ProductImg\ProductImgRepository;
+use App\Repositories\ProductImg\ProductImgRepositoryInterface;
+use App\Repositories\Review\ReviewRepository;
+use App\Repositories\Review\ReviewRepositoryInterface;
+use App\Repositories\User\UserRepository;
+use App\Repositories\User\UserRepositoryInterface;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
@@ -40,6 +56,42 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton(
             CartItemRepositoryInterface::class,
             CartItemRepository::class
+        );
+        $this->app->singleton(
+            AddressRepositoryInterface::class,
+            AddressRepository::class
+        );
+        $this->app->singleton(
+            BrandRepoInterface::class,
+            BrandRepo::class
+        );
+        $this->app->singleton(
+            CartItemRepositoryInterface::class,
+            CartItemRepository::class
+        );
+        $this->app->singleton(
+            OrderRepositoryInterface::class,
+            OrderRepository::class
+        );
+        $this->app->singleton(
+            OrderDetailRepositoryInterface::class,
+            OrderDetailRepository::class
+        );
+        $this->app->singleton(
+            PaymentRepositoryInterface::class,
+            PaymentRepository::class
+        );
+        $this->app->singleton(
+            ProductImgRepositoryInterface::class,
+            ProductImgRepository::class
+        );
+        $this->app->singleton(
+            ReviewRepositoryInterface::class,
+            ReviewRepository::class
+        );
+        $this->app->singleton(
+            UserRepositoryInterface::class,
+            UserRepository::class
         );
 
     }
